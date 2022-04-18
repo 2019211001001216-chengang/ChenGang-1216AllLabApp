39f2641c6f88d572417e001ccf398be4786391be
songph <1844349615@qq.com>
2022-04-18 23:01:35 +0800

    Lab 2 commit

---
+++ LabAss3/frmCustomerDataEntry.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace LabAss3
+{
+    partial class frmCustomerDataEntry
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkReading = new System.Windows.Forms.CheckBox();
+            this.chkPainting = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioUnmamied = new System.Windows.Forms.RadioButton();
+            this.radioMamied = new System.Windows.Forms.RadioButton();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.cmbCountry = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(50, 8);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(111, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Customer Name";
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(52, 47);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(63, 15);
+            this.lblCountry.TabIndex = 1;
+            this.lblCountry.Text = "Country";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(171, 8);
+            this.txtCustomerName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(89, 25);
+            this.txtCustomerName.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioFemale);
+            this.groupBox1.Controls.Add(this.radioMale);
+            this.groupBox1.Location = new System.Drawing.Point(55, 95);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(284, 83);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender";
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(162, 38);
+            this.radioFemale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(76, 19);
+            this.radioFemale.TabIndex = 1;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(14, 38);
+            this.radioMale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(60, 19);
+            this.radioMale.TabIndex = 0;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hobbies";
+            // 
+            // chkReading
+            // 
+            this.chkReading.AutoSize = true;
+            this.chkReading.Location = new System.Drawing.Point(134, 192);
+            this.chkReading.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkReading.Name = "chkReading";
+            this.chkReading.Size = new System.Drawing.Size(85, 19);
+            this.chkReading.TabIndex = 6;
+            this.chkReading.Text = "Reading";
+            this.chkReading.UseVisualStyleBackColor = true;
+            // 
+            // chkPainting
+            // 
+            this.chkPainting.AutoSize = true;
+            this.chkPainting.Location = new System.Drawing.Point(276, 192);
+            this.chkPainting.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkPainting.Name = "chkPainting";
+            this.chkPainting.Size = new System.Drawing.Size(93, 19);
+            this.chkPainting.TabIndex = 7;
+            this.chkPainting.Text = "Painting";
+            this.chkPainting.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioUnmamied);
+            this.groupBox2.Controls.Add(this.radioMamied);
+            this.groupBox2.Location = new System.Drawing.Point(55, 226);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(284, 83);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Status";
+            // 
+            // radioUnmamied
+            // 
+            this.radioUnmamied.AutoSize = true;
+            this.radioUnmamied.Location = new System.Drawing.Point(162, 38);
+            this.radioUnmamied.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioUnmamied.Name = "radioUnmamied";
+            this.radioUnmamied.Size = new System.Drawing.Size(92, 19);
+            this.radioUnmamied.TabIndex = 1;
+            this.radioUnmamied.TabStop = true;
+            this.radioUnmamied.Text = "Unmamied";
+            this.radioUnmamied.UseVisualStyleBackColor = true;
+            this.radioUnmamied.CheckedChanged += new System.EventHandler(this.radioUnmamied_CheckedChanged);
+            // 
+            // radioMamied
+            // 
+            this.radioMamied.AutoSize = true;
+            this.radioMamied.Location = new System.Drawing.Point(14, 38);
+            this.radioMamied.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioMamied.Name = "radioMamied";
+            this.radioMamied.Size = new System.Drawing.Size(76, 19);
+            this.radioMamied.TabIndex = 0;
+            this.radioMamied.TabStop = true;
+            this.radioMamied.Text = "Mamied";
+            this.radioMamied.UseVisualStyleBackColor = true;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(101, 314);
+            this.btnPreview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(113, 33);
+            this.btnPreview.TabIndex = 9;
+            this.btnPreview.Text = "Preview";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // cmbCountry
+            // 
+            this.cmbCountry.FormattingEnabled = true;
+            this.cmbCountry.Location = new System.Drawing.Point(171, 47);
+            this.cmbCountry.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbCountry.Name = "cmbCountry";
+            this.cmbCountry.Size = new System.Drawing.Size(108, 23);
+            this.cmbCountry.TabIndex = 10;
+            // 
+            // frmCustomerDataEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 375);
+            this.Controls.Add(this.cmbCountry);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.chkPainting);
+            this.Controls.Add(this.chkReading);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmCustomerDataEntry";
+            this.Text = "frmCustomerDataEntry";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCountry;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkReading;
+        private System.Windows.Forms.CheckBox chkPainting;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioUnmamied;
+        private System.Windows.Forms.RadioButton radioMamied;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.ComboBox cmbCountry;
+    }
+}
+
